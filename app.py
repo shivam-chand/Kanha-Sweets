5c955ec5773093dc0cbd8c66377cc8ed7da76729
shivam-chand <shivamchand.2013@gmail.com>
2026-05-26 17:22:11 +0530

    updated website

--- app.py
+++ app.py
@@ -1,30 +1,47 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-# Home page
+app.config["MONGO_URI"] = "mongodb+srv://shivamchand2013_db_user:9431176854ss@cluster1.eyur0v0.mongodb.net/kanha?retryWrites=true&w=majority&appName=Cluster1"
+
+mongo = PyMongo(app)
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Admin page
 @app.route('/admin')
 def admin():
-    return render_template('admin.html')
+    products = mongo.db.products.find()
+    return render_template('admin.html', products=products)
 
-# Add product page
 @app.route('/add-product', methods=['GET', 'POST'])
 def add_product():
+
     if request.method == 'POST':
+
         product_name = request.form['product']
         price = request.form['price']
 
-        print("New Product:", product_name)
-        print("Price:", price)
+        mongo.db.products.insert_one({
+            'name': product_name,
+            'price': price
+        })
 
         return redirect(url_for('admin'))
 
     return render_template('add_product.html')
+
+@app.route('/delete-product/<product_id>')
+def delete_product(product_id):
+
+    mongo.db.products.delete_one({
+        '_id': ObjectId(product_id)
+    })
+
+    return redirect(url_for('admin'))
 
 if __name__ == '__main__':
     app.run(debug=True)
